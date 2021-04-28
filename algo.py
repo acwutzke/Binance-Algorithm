6cdbee9316f6528f931b80c4b002d3ca54062f04
Alex Wutzke <acharleswutzke@gmail.com>
2021-04-28 15:20:41 -0600

    add supporting functions build the main body of the script

---
+++ algo.py
@@ -0,0 +1,135 @@
+
+from binance.websockets import BinanceSocketManager
+import websocket, json, numpy, pandas
+import config
+from algo_functions import *
+from binance.client import Client
+
+client = Client(config.API_KEY, config.API_SECRET)
+bm = BinanceSocketManager(client)
+
+WATCH_LIST=['dogeusdt@kline_1m', 'btcusdt@kline_1m', 'ethusdt@kline_1m','ltcusdt@kline_1m']
+
+# total portfolio size of 500 USD and max 2 positions
+CASH=500.00000
+print(CASH)
+max_pos=2
+
+portfolio= {}
+
+# instantiate portfolio
+for w in WATCH_LIST:
+	portfolio[w]={}
+	portfolio[w]['prices']=[]
+	portfolio[w]['in_position']=False
+	portfolio[w]['stop_price']=0
+	portfolio[w]['quantity']=0
+	portfolio[w]['bought_price']=0
+	portfolio[w]['start']=True #to indicate that the program has just started
+
+
+def on_message(message):
+	global CASH, max_pos
+	
+	# get info from message and portfolio
+	num_positions=num_pos(portfolio)
+	stream=message['stream']
+	symbol=stream[:len(stream)-9].upper()
+	close=float(message['data']['k']['c'])
+	stop=portfolio[stream]['stop_price']
+	bought_price=portfolio[stream]['bought_price']
+	in_position=portfolio[stream]['in_position']
+	quantity=portfolio[stream]['quantity']
+	start=portfolio[stream]['start']
+	profit=0.000000
+
+	# append close and trim
+	portfolio[stream]['prices'].append(float(close)) # put this in an if statement with in_position
+	portfolio[stream]['prices']=portfolio[stream]['prices'][-300:]
+
+	# calculate emas
+	if len(portfolio[stream]['prices'])>50:
+		ema10=ema(portfolio[stream]['prices'],10)
+		ema30=ema(portfolio[stream]['prices'],30)
+
+		# print(stream,start,ema10,ema30)
+
+		if start and ema10<ema30:
+			portfolio[stream]['start']=False
+	else:
+		print('Not enough data')
+		return
+
+	# check close against stop price
+	if in_position and close<stop:
+		# order_successful=sell(symbol,quantity)
+		CASH=CASH+(close*quantity)
+		profit=quantity*(close-bought_price)
+		order_successful=True
+		if order_successful:
+			portfolio[stream]['in_position']=False
+			portfolio[stream]['stop_price']=0
+			portfolio[stream]['quantity']=0
+			portfolio[stream]['bought_price']=0
+			log_message=['Sell',symbol,quantity,stop,'STOP',profit]
+			print(log_message)
+			print(CASH)
+			holdings(portfolio)
+
+		else:
+			print('Failed to execute sale on stop price...')
+		return
+
+	# check whether ema10 has decreased below ema30
+	if in_position:
+		if ema10<ema30:
+			# order_successful=sell(symbol,quantity)
+			CASH=CASH+(close*quantity)
+			profit=quantity*(close-bought_price)
+			order_successful=True
+			if order_successful:
+				portfolio[stream]['in_position']=False
+				portfolio[stream]['stop_price']=0
+				portfolio[stream]['quantity']=0
+				portfolio[stream]['bought_price']=0
+				log_message=['Sell',symbol,quantity,close,'EMA',profit]
+				print(log_message)
+				print(CASH)
+				holdings(portfolio)
+			else:
+				print('Failed to execute sale on EMA...')
+			return
+
+	# check whether ema10 has increased above ema30
+	else:
+		if ema10>ema30 and num_positions<max_pos and start==False:
+			buy_quantity=CASH/close/(max_pos-num_positions)
+			CASH=CASH-buy_quantity*close
+			# order_successful=buy(symbol,buy_quantity)
+			order_successful=True
+			if order_successful:
+				portfolio[stream]['in_position']=True
+				portfolio[stream]['stop_price']=ema30
+				portfolio[stream]['quantity']=buy_quantity
+				portfolio[stream]['bought_price']=close
+				CASH=CASH
+				log_message=['Buy',symbol,buy_quantity,close,'EMA']
+				print(log_message)
+				print(CASH)
+				holdings(portfolio)
+			else:
+				print('Failed to buy on EMA...')
+			return
+
+
+
+
+# start any sockets here, i.e a trade socket
+conn_key = bm.start_multiplex_socket(WATCH_LIST, on_message)
+# then start the socket manager
+bm.start()
+
+"""
+Need to fix it so that coins are in wait mode so that we only buy on the cross.
+"""
+
---
+++ algo_functions.py
@@ -0,0 +1,71 @@
+import datetime
+
+def num_pos(portfolio):
+	count=0
+	for p in portfolio:
+		if portfolio[p]['in_position']:
+			count+=1
+	return count
+
+def ema(prices,length):
+	init=sum(prices[:length])/length
+	ema=[init]
+	mult=(2/(length+1))
+
+	for i in range(length,len(prices)):
+		current_ema=(prices[i]-ema[i-length])*mult+ema[i-length]
+		ema.append(current_ema)
+	return ema[-1:][0]
+
+def sell(stream,quantity):
+	order_successful = order(SIDE_SELL, quantity, stream)
+	if order_successful:
+		return True
+	else:
+		return False
+    	
+def buy(stream,quantity):
+	order_successful = order(SIDE_BUY, quantity, stream)
+	if order_successful:
+		return True
+	else:
+		return False
+
+# def order(side, quantity, symbol,order_type=ORDER_TYPE_MARKET):
+#     try:
+#         print("sending order")
+#         order = client.create_order(symbol=symbol, side=side, type=order_type, quantity=quantity)
+#         print(order)
+#     except Exception as e:
+#         print("an exception occured - {}".format(e))
+#         return False
+#     return True
+
+def algo_log(text):
+    time=str(datetime.datetime.now())[:16]
+    text=time+' : '+str(text)
+    with open(r'C:\Users\Alex\Desktop\Binance-Algorithm\log.txt', 'a') as f:
+        f.write(text)
+        f.write('\n')
+
+def holdings(portfolio):
+	hold={}
+	for p in portfolio:
+		if portfolio[p]['in_position']:
+				hold[p]=[portfolio[p]['quantity'],portfolio[p]['bought_price']]
+
+	print(hold)
+
+
+
+
+
+
+
+
+
+
+		
+
+
+
