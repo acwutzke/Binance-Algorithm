e061b9335276d3548ca070cc43def6cb94e18e8c
Alex Wutzke <acharleswutzke@gmail.com>
2021-05-10 13:05:08 -0600

    import binance enums so that trades will execute properly

--- algo.py
+++ algo.py
@@ -4,6 +4,7 @@ import websocket, json, numpy, pandas, datetime
 import config
 from algo_functions import *
 from binance.client import Client
+from binance.enums import *
 
 client = Client(config.API_KEY, config.API_SECRET)
 bm = BinanceSocketManager(client)
